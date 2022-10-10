154d663fc547995b7e772dddd7297f71ab1bac81
thydo <thydo@asu.edu>
2022-10-10 15:23:45 -0700

    config update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import javax.swing.JFrame;
 
-public class Init {
+public class Main {
 
-	
-	public static void Main (String[] args)
+	public static void main (String[] args)
 	{
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
