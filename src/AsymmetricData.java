af90b730ebb18943ec201bbaf139e73bfd7b4b9a
thydo <thydo@asu.edu>
2022-10-10 23:31:49 -0700

    asym data table output fixed

--- src/AsymmetricData.java
+++ src/AsymmetricData.java
@@ -22,16 +22,33 @@ public class AsymmetricData extends Data implements MakeGraph{
     public void GetData(String dir, int dimension) throws IOException
     {
     	dataPoints = super.GetDataPoints(dir);
-    	dataPoints.forEach(item -> {System.out.println (item);});
-    	System.out.println(dataPoints.get(0));
+    	//dataPoints.forEach(item -> {System.out.println (item);});
+    	//System.out.println(dataPoints.get(0));
         this.dimension = dimension;
         makeAdjacencyMatrix();
     }
     
     public double[][] makeAdjacencyMatrix(){
-        int cities = this.dataPoints.size();
-        double[][] tsp = new double[cities][cities];
-
+        ArrayList<Double> elements = new ArrayList<Double>();
+        dataPoints.forEach(line -> {
+        	line = line.strip();
+        	String[] e = line.split("\\s+");
+        	for (int i = 0; i < e.length; i++)
+        	{
+        		elements.add(Double.parseDouble(e[i]));
+        	}
+        });
+                
+        double[][] tsp = new double[dimension][dimension];
+        for (int i = 0; i < dimension; i++)
+        {
+        	for (int j = 0; j < dimension; j++)
+        	{
+        		tsp[i][j] = elements.get((i*17)+j);
+        	}
+        }
+        
+        
         return tsp;
     }
 }
