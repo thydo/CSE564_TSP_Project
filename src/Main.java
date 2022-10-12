5c04155a9bce80234270ea81096fae65227f6f3e
Sannidhya Pathania <pathaniasannidhya@gmail.com>
2022-10-11 18:15:28 -0700

    Java Documentation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 
 import java.io.IOException;
 
+/**
+ * The Main class is the driver class which instantiates an object of Graphics
+ * class that interactively asks the user to choose from given options
+ * displayed on screen.
+*/
 public class Main {
+
+	/**
+	 * main method is the driver method to instantiate an object of Graphics class
+	 * 
+	 * @throws IOException - signals if any IO exception occurred while reading
+	 * the files.
+	*/
 	public static void main (String[] args) throws IOException {
 		Graphics graphics = new Graphics();
 		graphics.Start();
